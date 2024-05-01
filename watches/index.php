2b418da4ea98bd48fe3d212a695d599f29072510
Maaz0313 <maaztajammul63@gmail.com>
2024-05-01 16:15:49 +0500

    small tweak

--- watches/index.php
+++ watches/index.php
@@ -493,7 +493,7 @@ if (isset($_SESSION['status'])) {
                                                         <li> <?= $row['touchscreen'] == 1 ? 'Touchscreen' : 'No Touchscreen' ?></li>
                                                         <li> <?= $row['screen_size'] ?> Display</li>
                                                         <li> <?= $row['battery_life'] ?></li>
-                                                        <li> <?= $row['features'] ?></li>
+                                                        <li> <?= isset($row['water_resistant']) ? 'Water Resistant' : 'No Water Resistant' ?></li>
                                                         <li> <?= $row['extra_features'] ?></li>
                                                         <li> <?= $row['os'] ?></li>
                                                     </ul>
